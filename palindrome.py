a91b89b8d9d525d1a3ee6b124a24ef1b2a0ad7e7
christopherwburke <christopher.w.burke@gmail.com>
2019-06-04 21:27:36 -0400

    starting to see the light.

---
+++ palindrome.py
@@ -0,0 +1,40 @@
+#Write a program that asks the user for one or more sentences and then lets the user know if it is a palindrome.
+
+#ask user for string input
+
+word = input("Enter a word: ")
+
+#remove empty space characters, numbers, punctuation
+
+#evaluate that word to see if the first letter in the word is the same as the last letter in the word, and so on
+
+#racecar
+
+word_length = len(word)
+
+#5 letters
+# 0, -1 2/3
+# 1, -2 4/5
+# 2, -3 6/7
+# 3, -4
+
+def checkpalindrome(word, word_length):
+    x = 0
+    y = -1
+    for idx in range(int(word_length/2)):
+        if word[x] != word[y]:
+            print("Not A Palindrome")
+            return
+        else:
+            x+=1
+            y-=1 
+    print("Palindrome")
+
+checkpalindrome(word, word_length)
+
+
+# so is palindrome[0] = palindrome [-1]
+
+# ? return true if palindrome and false if not palindrome?
+
+# then print to screen whether palindrome or not
