2ef85f6ad99a7dd6a7459f9478f08e818be01da6
christopherwburke <christopher.w.burke@gmail.com>
2019-06-12 00:13:56 -0400

    Getting caught up

--- palindrome.py
+++ palindrome.py
@@ -4,13 +4,28 @@
 
 word = input("Enter a word: ")
 
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+empty_list = []
+
+for letter in word.lower():
+    if letter in letters:
+        empty_list.append(letter)
+
+reverse_list = empty_list[::-1]
+
+if empty_list == reverse_list:
+    print(word + " is a palindrome!")
+else:
+    print(word + " is NOT a palindrome!")
+
 #remove empty space characters, numbers, punctuation
 
 #evaluate that word to see if the first letter in the word is the same as the last letter in the word, and so on
 
 #racecar
 
-word_length = len(word)
+# word_length = len(word)
 
 #5 letters
 # 0, -1 2/3
@@ -18,19 +33,19 @@ word_length = len(word)
 # 2, -3 6/7
 # 3, -4
 
-def checkpalindrome(word, word_length):
-    x = 0
-    y = -1
-    for idx in range(int(word_length/2)):
-        if word[x] != word[y]:
-            print("Not A Palindrome")
-            return
-        else:
-            x+=1
-            y-=1 
-    print("Palindrome")
+# # def checkpalindrome(word, word_length):
+#     x = 0
+#     y = -1
+#     for idx in range(int(word_length/2)):
+#         if word[x] != word[y]:
+#             print("Not A Palindrome")
+#             return
+#         else:
+#             x+=1
+#             y-=1 
+#     print("Palindrome")
 
-checkpalindrome(word, word_length)
+# checkpalindrome(word, word_length)
 
 
 # so is palindrome[0] = palindrome [-1]
